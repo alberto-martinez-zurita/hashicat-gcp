1a7dae0fafe5ece5cb9719f21e1accccbc62b33f
alberto-martinez-zurita <albertomz@gmail.com>
2023-05-16 14:30:18 +0000

    Added vpc module

--- main.tf
+++ main.tf
@@ -50,8 +50,9 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-billable =true
-department ="devops"
+  billable = true
+  department = "devops"
+
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
