58c7c5893c2828ccfefd3b327a665bfc6f4ecc72
alberto-martinez-zurita <albertomz@gmail.com>
2023-05-16 14:16:40 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,20 +1,18 @@
 module "vpc" {
-    source  = "alberto-martinez-zurita/network/google"
-    version = "3.4.0"
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
 
-
-    network_name   = "gaurav-network"
-    project_id = var.project
+    project_id   = var.project
+    network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
-   subnets = [
+subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
-
 
     routes = [
         {
